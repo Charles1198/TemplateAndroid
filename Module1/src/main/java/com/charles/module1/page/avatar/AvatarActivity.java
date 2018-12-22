4d9f2a874c178b7b35e6d7a9b32861c0e9b9f06d
charles <charles@charlesdeMacBook-Pro.local>
2018-12-22 13:27:25 +0800

    增加换头像功能

---
+++ Module1/src/main/java/com/charles/module1/page/avatar/AvatarActivity.java
@@ -0,0 +1,18 @@
+package com.charles.module1.page;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.charles.module1.R;
+
+/**
+ * @author charles
+ */
+public class AvatarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.module1_activity_avatar);
+    }
+}
